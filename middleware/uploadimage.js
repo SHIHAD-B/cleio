cf7901666e65bb755863085d7584b23d00f5df67
SHIHAD-B <shihadsafe@gmail.com>
2023-12-11 19:07:58 +0530

    error corrected

--- middleware/uploadimage.js
+++ middleware/uploadimage.js
@@ -1,25 +1,17 @@
 const multer = require("multer");
 const crypto = require("crypto");
-const fs = require("fs");
-
-const destinationFolder = "./public/productimage/";
-
-if (!fs.existsSync(destinationFolder)) {
-    fs.mkdirSync(destinationFolder, { recursive: true });
-}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, destinationFolder);
+        cb(null, "./Public/productimage/");
     },
     filename: function (req, file, cb) {
-        const randomString = crypto.randomBytes(3).toString("hex");
+        const randomeString = crypto.randomBytes(3).toString("hex");
         const timestamp = Date.now();
-        const uniqueFile = `${timestamp}-${randomString}`;
+        const uniqueFile = `${timestamp}-${randomeString}`;
         cb(null, uniqueFile + ".png");
     },
 });
-
 const upload = multer({ storage: storage });
 
 module.exports = upload;
