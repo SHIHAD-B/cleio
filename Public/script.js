c180e9ee617dc687b9100e93d83b645eb4ca518b
SHIHAD-B <shihadsafe@gmail.com>
2023-12-15 17:09:15 +0530

    multer limit issue fixed

--- Public/script.js
+++ Public/script.js
@@ -2,11 +2,13 @@ const image = document.getElementById('edit_image')
 alert(image.src)
 const cropper = new Cropper(image, {
     aspectRatio: 1 / 1,
-    viewMode: 1
+    viewMode: 0
 });
 
 document.querySelector('#btn-crop').addEventListener('click', function () {
-    const croppedImage = cropper.getCroppedCanvas({ width: 500, height: 500 }).toDataURL('image/png');
+    var croppedImage = cropper.getCroppedCanvas().toDataURL('image/png');
     document.getElementById('previewImage').src = croppedImage;
-});
+
+})
+
 
--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var instance = new Razorpay({
 
 app.set("view engine", "ejs");
 app.use(nocache());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }))
 app.use(express.static('Public'));
 
 app.use(session({
