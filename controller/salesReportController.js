da8ae57a9d9007f211b7fe4fb9f8f9f91c44c996
SHIHAD-B <shihadsafe@gmail.com>
2023-12-15 12:00:29 +0530

    error not defined 3

--- controller/salesReportController.js
+++ controller/salesReportController.js
@@ -901,13 +901,13 @@ const generateOverallReport = async (req, res, next) => {
         pdf.create(htmlContent, options).toFile((err, file) => {
             if (err) {
                 console.log(err);
-                return next(err)
+
             }
 
             res.download(file.filename, 'overall_report.pdf', (err) => {
                 if (err) {
                     console.log(err);
-                    return next(err)
+
                 }
 
                 fs.unlinkSync(file.filename);
